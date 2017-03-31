78f4c4c2b61ff83369f02499d5e02ab2432610b1
SeleniumAutomationGitHub <seleniumautomaton@gmail.com>
2017-03-31 20:28:25 +0530

    AutoCompletFromSearchField method implemented

--- Email/src/com/emaimail/EmailTest.java
+++ Email/src/com/emaimail/EmailTest.java
@@ -1,24 +1,47 @@
 package com.emaimail;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 
 public class EmailTest {
 	
-	WebDriver wd;
-  @Test
-  public void f() {
-	  
-	  System.out.println("URL opened");
-	  System.out.println(wd.getTitle());
+	WebDriver driver;
+	WebDriverWait wdw;
+		
+  @Test (description="AutoCompletFromSearchField", priority=1)
+  public void verifyAutoCompleteText() throws Exception{
+	  wdw = new WebDriverWait(driver, 60);
+	  driver.findElement(By.xpath(".//*[@id='sb_ifc0']")).sendKeys("test");
+	  WebElement allTxts = driver.findElement(By.xpath("//*[@class='sbdd_b']//ul[@class='sbsb_b']"));
+	  			//Identifying all the search result elements by this only one locator
+	  wdw.until(ExpectedConditions.visibilityOf(allTxts));
+	  //List<WebElement> allTxt = allTxts.findElements(By.tagName("li"));
+	  List<WebElement> allTxt = allTxts.findElements(By.tagName("li"));
+	  			//Identify each search result locator by this tagName from above locator
+	  for(WebElement eachElement : allTxt){
+		  System.out.println(eachElement.getText());
+		  if(eachElement.getText().equalsIgnoreCase("test ranking")){
+			  eachElement.click();
+			  break;
+		  }
+	} 
   }
+  
+  
+  
+  
   @BeforeTest
   public void beforeTest() {
-	  
-	  wd = new FirefoxDriver();
-	  wd.get("https://www.google.com/");
+	  driver = new FirefoxDriver();
+	  driver.get("https://www.google.co.in/");
   }
 
 }
